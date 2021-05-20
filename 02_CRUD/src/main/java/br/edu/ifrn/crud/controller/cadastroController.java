be7c824c0de40fa6dcf3bac78b958183dc646245
Luanzeixon <luananjos765@gmail.com>
2021-05-20 13:39:51 -0300

    Parte_02_Cadastro_CorrecaoErros
    Apenas algumas correções no codigo, como o uso do redirect no return

--- 02_CRUD/src/main/java/br/edu/ifrn/crud/controller/cadastroController.java
+++ 02_CRUD/src/main/java/br/edu/ifrn/crud/controller/cadastroController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifrn.crud.dominio.Usuario;
 
@@ -27,10 +27,11 @@ public class cadastroController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Usuario usuario, HttpSession sessao) { //já que nao tem BD o usuario vai ser salvo na memoria(seção do servidorpor isso o httpsession
+	public String salvar(Usuario usuario, RedirectAttributes attr, HttpSession sessao) { //já que nao tem BD o usuario vai ser salvo na memoria(seção do servidorpor isso o httpsession
+		//RedirectAttributes serve como um modelMap mas quando retorna redirect em vez de forward
 		//Pegando valores da sessão
-		Integer id = (Integer) sessao.getAttribute("id");
-		List<Usuario> usuariosCadastrados = (List<Usuario>) sessao.getAttribute("usuariosCadastrados");
+		Integer id = (Integer) sessao.getAttribute("id"); //pesquisar na memoria(sessao) se tem algum id igual o id colocado no form
+		List<Usuario> usuariosCadastrados = (List<Usuario>) sessao.getAttribute("usuariosCadastrados"); // ver se tem alguma lista de usuraios existente na memoria
 		
 		if(id == null) { //1 id cadastrado
 			id = 1;
@@ -42,11 +43,13 @@ public class cadastroController {
 		usuariosCadastrados.add(usuario); //adicionar o usuario ao array de usuario
 		id++;
 		
-		sessao.setAttribute("id", id); //salvar na memoria
-		sessao.setAttribute("usuariosCadastrados", usuariosCadastrados); //salvar na memoria
+		sessao.setAttribute("id", id); //salvar na memoria o novo id para ser pesquisado na linha 33(comeco do codigo slavar)
+		sessao.setAttribute("usuariosCadastrados", usuariosCadastrados); //salvar na memoria a lista de usuarios
 		
+		attr.addFlashAttribute("msgSucesso", "Cadastro realizado com sucesso!");
+		//Tem que ser Flash pois é um atributo temporario.
 		
-		return "usuario/cadastro";
+		return "redirect:/usuarios/cadastro"; //usar redirect na url usuarios, para "apagar" os contatos salvos do form (tem que colocar /)
 	}
 	
 	@ModelAttribute("profissoes")
